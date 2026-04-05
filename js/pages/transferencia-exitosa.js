b5aa2e8fcb908e00e774190121dc5301043a0f46
Carolina Bedoya Rendón <aonixsoul@gmail.com>
2026-04-04 19:03:58 -0500

    fix: se implementa timer en transferencias

--- js/pages/transferencia-exitosa.js
+++ js/pages/transferencia-exitosa.js
@@ -3,8 +3,18 @@
    ========================================= */
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Redirigir después de 3 segundos
-    setTimeout(() => {
-        window.location.href = 'dashboard.html';
-    }, 4000);
+    const countdownEl = document.querySelector('#countdown');
+    const dashboardRedirect = 'dashboard.html';
+
+    let timeLeft = 20;
+    const interval = setInterval(() => {
+        if (!countdownEl) return;
+        timeLeft -= 1;
+        countdownEl.textContent = timeLeft;
+
+        if (timeLeft <= 0) {
+            clearInterval(interval);
+            window.location.href = dashboardRedirect;
+        }
+    }, 1000);
 });
